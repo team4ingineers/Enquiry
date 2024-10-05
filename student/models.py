32feb8baceff2c301805f36c5b2cea5d7d39b304
yashpunmiya <yyash7379@gmail.com>
2024-10-06 03:19:02 +0530

    college repair

--- student/models.py
+++ student/models.py
@@ -8,7 +8,6 @@ class Student(models.Model):
     def __str__(self):
         return self.user.username
 
-
 class Enquiry(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
@@ -25,6 +24,8 @@ class Enquiry(models.Model):
 
     def __str__(self):
         return f"Enquiry from {self.student.user.username} to {self.college.user.username}"
+
+
         
         
         
