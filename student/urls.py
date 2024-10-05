27266d6af3d8c49b7586270bf6cb8693739a395d
yashpunmiya <yyash7379@gmail.com>
2024-10-05 16:43:09 +0530

    yash update

--- student/urls.py
+++ student/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('signup/', student_signup, name='student_signup'),
     path('student_dashboard', student_dashboard_view, name='student_dashboard_view'),
+     path('tour/<int:id>/', college_tour, name='college_tour'),
 ]
