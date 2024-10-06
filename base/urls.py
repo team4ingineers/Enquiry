620b4631368354c2658969dffff3752319d851d6
Vinish Rexson <vinishrexson2005@gmail.com>
2024-10-06 11:18:05 +0530

    add tabs

--- base/urls.py
+++ base/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,7 @@ urlpatterns = [
     path('college/', include('college.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', home ,name= 'home'),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='home'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
